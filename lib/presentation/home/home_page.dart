dcc364be40698ba13942764caef5c4291098fb36
xrole <xrolediamond@gmail.com>
2023-01-10 18:31:10 +0100

    implement mvvm architecture

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:ucha/presentation/base/base_view.dart';
+import 'package:ucha/presentation/home/home_view_model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Use Ucha Today'),));
+    return  Scaffold(
+      body: BaseView<HomeViewModel>(
+        model: HomeViewModel(),
+        onModelReady: (model) => model.init(),
+        builder: (context, model, _) {
+          return const Center(child: Text('Use Ucha Today'),);
+        }
+      ));
   }
 }
