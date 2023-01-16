565bf3ae6fa3927f0fcc2116f1a096a637a90b9d
xrole <xrolediamond@gmail.com>
2023-01-16 18:33:42 +0100

    home page

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ucha/presentation/base/base_view.dart';
 import 'package:ucha/presentation/home/home_view_model.dart';
+import 'package:ucha/presentation/home/widgets.dart/description.dart';
 import 'package:ucha/presentation/widgets/buttons/app_button.dart';
+import 'package:ucha/presentation/widgets/unfocus_widget.dart';
+import 'package:ucha/utils/app_text_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,78 +12,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-        body: BaseView<HomeViewModel>(
-            model: HomeViewModel(),
-            onModelReady: (model) => model.init(),
-            builder: (context, model, _) {
-              return SingleChildScrollView(
-                child: SizedBox(
-                  height: size.height,
-                  child: Column(
-                    children: [
-                      Stack(
-                        children: [
-                          Container(
-                            height: size.height * .3,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: Image.asset("assets/img/bg.png").image,
-                                fit: BoxFit.cover,
-                              ),
+    return UnfocusWidget(
+      child: Scaffold(
+          body: BaseView<HomeViewModel>(
+              model: HomeViewModel(),
+              onModelReady: (model) => model.init(),
+              builder: (context, model, _) {
+                return SingleChildScrollView(
+                  child: Container(
+                    height: size.height,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  height: size.height * .3,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: Image.asset("assets/img/bg.png").image,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: size.height * .3,
+                                  width: double.infinity,
+                                  color: Colors.black.withOpacity(.5),
+                                ),
+                                Container(
+                                  height: size.height * .3,
+                                  width: double.infinity,
+                                  child: const Center(
+                                      child: Text(
+                                    "Ucha",
+                                    style: TextStyle(
+                                        fontSize: 50,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                                ),
+                              ],
                             ),
-                          ),
-                          Container(
-                            height: size.height * .3,
-                            width: double.infinity,
-                            color: Colors.black.withOpacity(.5),
-                          ),
-                          Container(
-                            height: size.height * .3,
-                            width: double.infinity,
-                            child: const Center(
-                                child: Text(
-                              "Ucha",
-                              style: TextStyle(
-                                  fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),
-                            )),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        height: size.height * .7,
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                          Column(
-                            children: [
-                              TextFormField(),
-                              TextFormField(),
-                              TextFormField(),
-                              TextFormField(),
-                              TextFormField(),
-                              TextFormField(),
-                              TextFormField(),
-                              TextFormField(),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              AppButton(
-                                title: "Get Quote",
-                                onTap: () {
-                                  print("Hey, you tapped me");
-                                },
+                           const SizedBox(height: 30,),
+                           Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:  [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                child: Text("WB calculator".toUpperCase(), style: AppTextStyle.subTitle(color: Colors.black.withOpacity(.7)),),
                               ),
-                              const SizedBox(height: 50),
+                            DescriptionForm(),
                             ],
-                          )
-                        ]),
-                      ),
-                    ],
+                           ),
+                          ],
+                        ),
+                        Padding(
+                          padding:  EdgeInsets.only(top: 20, right: 20, bottom: size.height * .07, left:  20),
+                          child: AppButton(
+                            title: "Get Quote",
+                            onTap: () {
+                              print("Hey, you tapped me");
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }));
+                );
+              })),
+    );
   }
 }
--- lib/utils/app_text_style.dart
+++ lib/utils/app_text_style.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppTextStyle {
+  static TextStyle subTitle({
+    double? fontSize,
+    Color? color,
+  }) {
+    return TextStyle(
+      fontSize: fontSize ?? 16,
+      fontWeight: FontWeight.bold,
+      color: color 
+    );
+  }
   static TextStyle medium({
     double? fontSize,
     Color? color,
